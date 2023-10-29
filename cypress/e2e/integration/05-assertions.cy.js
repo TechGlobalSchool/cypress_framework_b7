f42589bc730c7f45e992228f8d41996770e79623
TechGlobalSchool <tgschool01@gmail.com>
2023-10-29 14:37:10 -0500

    Practice & Assertions Part-2

--- cypress/e2e/integration/05-assertions.cy.js
+++ cypress/e2e/integration/05-assertions.cy.js
@@ -70,13 +70,91 @@ describe('Cypress Assertions', () => {
 
 
 
+
         // cy.get('#hello_paragraph').should('have.text', 'Hello World!')
         // cy.get('#hello_paragraph').should('have.attr', 'id', 'hello_paragraph')
         // cy.get('#hello_paragraph').should('be.visible')
 
-        
+
         cy.get('#hello_paragraph').should('have.text', 'Hello World!')
         .and('have.attr', 'id', 'hello_paragraph')
         .and('be.visible')
+
+
+        cy.visit('https://techglobal-training.com/frontend/project-4')
+
+        cy.get('#add_product_btn').click()
+        cy.get('.delete').click()
+
+        // NOTE: Use 'not.exist' chainer to validate web element disappeared from the DOM
+        cy.get('.modal-card').should('not.exist')
     })
+
+
+    it.only('Explicit Assertions', () => {
+
+        cy.visit('https://techglobal-training.com/frontend')
+        cy.get('.cards').contains('Html Elements').click()
+
+        // Any assertion we do using .should() method, is an "Implicit Assertion"
+
+
+
+        // be.visible checks if the web element displayed on the UI
+        cy.get('#main_heading').should('have.text', 'Html Elements')
+
+        cy.get('#main_heading').invoke('text').then((ele) => {
+            const el = ele
+            cy.log(el)
+            expect(el).to.equal('Html Elements')
+        })
+
+        cy.get('#main_heading').then(($ele) => {
+            const el = $ele.text()
+            expect(el).to.equal('Html Elements')
+        })
+
+        cy.get('#main_heading').then(($ele) => {
+            const el = $ele.text()
+            expect(el).to.include('Elements')
+        })
+
+        cy.get('#main_heading').then(($ele) => {
+            const el = $ele
+            expect(el).to.exist
+        })
+
+
+        cy.get('#main_heading').invoke('attr', 'id').then((attr) => {
+            expect(attr).to.equal('main_heading')
+        })
+
+        cy.get('#main_heading').then(($ele) => {
+            expect($ele.attr('id')).to.eq('main_heading')
+        })
+
+        cy.get('[id*="paragraph"]').then(($el) => {
+            expect($el).to.have.length(2)
+        })
+
+        cy.get('#checkbox_1').then(($el) => {
+            expect($el).to.be.enabled
+            expect($el).to.be.visible
+            expect($el).not.to.be.checked
+        })
+
+        cy.get('[id*="paragraph"]').should('have.css', 'color', 'rgb(105, 105, 105)')
+
+        cy.get('[id*="paragraph"]').then(($el) => {
+            expect($el.css('color')).to.equal('rgb(105, 105, 105)')
+        })
+
+        cy.get('#main_heading').then(($el) => {
+            cy.wrap($el).should('have.text', 'Html Elements')
+            cy.wrap($el.text()).should('eq', 'Html Elements')
+        })
+
+})
+
+
 })
